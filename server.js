7b579ec43a53f701cf34bbd04a9b26553a4c734f
unknown <araf.chakib.m@gmail.com>
2023-09-26 22:30:15 +0200

    insert server

--- server.js
+++ server.js
@@ -1,28 +1,51 @@
+// Importez le module Express
 const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
+// Créez un nouvel objet Express
+const app = express();
+
+// Spécifiez l'emplacement du fichier pixel.js
 app.use("/pixel", express.static("./pixel"));
 
-app.post("/pixel/open", (req, res) => {
+// Définissez le chemin d'accès aux requêtes POST
+app.post("/pixel/open", handlePost);
+
+// Démarrez le serveur web
+app.listen(3000 );
+console.log("serveur open");
+
+// Fonction qui traite les requêtes POST
+function handlePost() {
     // Vérifiez que la requête est valide
-    if (req.headers["content-type"] !== "application/json") {
-        res.status(400).send("Invalid request");
-        return;
+    if (req.headers["Content-Type"] != "application/json") {
+      return "Invalid request";
     }
-
+  
     // Extrayez les données de la requête
-    const data = req.body;
-
+    const data = JSON.parse(req.body);
+  
     // Enregistrez les données
-    console.log("Pixel ouvert : action=%s, email_id=%s", data["action"], data["email_id"]);
-
+    console.log("Pixel ouvert : action=%s, email_id=%s, timestamp=%s", data["action"], data["email_id"], data["timestamp"]);
+  
     // Envoyez une réponse
-    res.status(200).send("OK");
-});
+    return "OK";
+  }
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Serveur en cours d'écoute sur le port ${port}`);
-});
+// Fonction qui est appelée lorsque l'image est chargée
+function onImageLoad() {
+    // Vérifiez que l'image a été chargée
+    const img = document.getElementById("pixel");
+    if (img.complete) {
+      // Envoyez une requête POST à votre serveur web
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://example.com/pixel/open");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(JSON.stringify({
+        action: "open_email",
+        email_id: "1234567890",
+      }));
+    
+      // Appelez une fonction qui vous avertit que l'e-mail a été ouvert
+      window.alert("L'e-mail a été ouvert !");
+    }
+  }
