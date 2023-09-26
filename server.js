9ba4cf6b55411dd07ed12bc0075bdc30393ecd80
unknown <araf.chakib.m@gmail.com>
2023-09-27 01:25:44 +0200

    correction server

--- server.js
+++ server.js
@@ -1,51 +1,58 @@
 // Importez le module Express
 const express = require("express");
+const bodyParser = require("body-parser");
 
-// Créez un nouvel objet Express
 const app = express();
+const port = 3000;
 
-// Spécifiez l'emplacement du fichier pixel.js
-app.use("/pixel", express.static("./pixel"));
+app.use(bodyParser.json());
 
-// Définissez le chemin d'accès aux requêtes POST
 app.post("/pixel/open", handlePost);
 
-// Démarrez le serveur web
-app.listen(3000 );
-console.log("serveur open");
+app.listen(port, () => {
+  console.log(`Serveur ouvert sur le port ${port}`);
+});
 
-// Fonction qui traite les requêtes POST
-function handlePost() {
-    // Vérifiez que la requête est valide
-    if (req.headers["Content-Type"] != "application/json") {
-      return "Invalid request";
-    }
-  
-    // Extrayez les données de la requête
-    const data = JSON.parse(req.body);
-  
-    // Enregistrez les données
-    console.log("Pixel ouvert : action=%s, email_id=%s, timestamp=%s", data["action"], data["email_id"], data["timestamp"]);
-  
-    // Envoyez une réponse
-    return "OK";
+function handlePost(req, res) {
+  // Vérifiez que la requête est valide
+  if (req.headers["content-type"] !== "application/json") {
+    return res.status(400).send("Requête invalide");
   }
 
-// Fonction qui est appelée lorsque l'image est chargée
+  // Extrayez les données de la requête
+  const data = req.body;
+
+  // Enregistrez les données
+  console.log(
+    "Pixel ouvert : action=%s, email_id=%s, timestamp=%s",
+    data["action"],
+    data["email_id"],
+    data["timestamp"]
+  );
+
+  // Envoyez une réponse
+  res.status(200).send("OK");
+}
+
 function onImageLoad() {
-    // Vérifiez que l'image a été chargée
     const img = document.getElementById("pixel");
     if (img.complete) {
-      // Envoyez une requête POST à votre serveur web
       const xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://chakibaraf.github.io/pixel/open");
+      xhr.open("POST", "http://localhost:3000/pixel/open"); // Utilisez l'URL du serveur
       xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(JSON.stringify({
-        action: "open_email",
-        email_id: "1234567890",
-      }));
-    
-      // Appelez une fonction qui vous avertit que l'e-mail a été ouvert
-      window.alert("L'e-mail a été ouvert !");
+      xhr.send(
+        JSON.stringify({
+          action: "open_email",
+          email_id: "1234567890",
+        })
+      );
+  
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          window.alert("L'e-mail a été ouvert !");
+        } else {
+          window.alert("Une erreur s'est produite.");
+        }
+      };
     }
   }
