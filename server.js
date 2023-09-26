a20e9ff18eeb03750b28f85fe92f80603053d602
unknown <araf.chakib.m@gmail.com>
2023-09-27 01:53:08 +0200

    correction server

--- server.js
+++ server.js
@@ -42,25 +42,3 @@ function handlePost(req, res) {
   res.status(200).send("OK");
 }
 
-function onImageLoad() {
-    const img = document.getElementById("pixel");
-    if (img.complete) {
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://chakibaraf.github.io/pixel/open"); // Utilisez l'URL du serveur
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(
-        JSON.stringify({
-          action: "open_email",
-          email_id: "1234567890",
-        })
-      );
-  
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          window.alert("L'e-mail a été ouvert !");
-        } else {
-          window.alert("Une erreur s'est produite.");
-        }
-      };
-    }
-  }
