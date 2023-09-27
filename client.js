ea119587e9dda445fc479b3f7ab340e687734680
unknown <araf.chakib.m@gmail.com>
2023-09-27 02:50:07 +0200

    correction server

--- client.js
+++ client.js
@@ -21,35 +21,38 @@
     }
   }*/
 
-  // Attendez que le document soit chargé
+ // Attendez que le document soit chargé
 document.addEventListener("DOMContentLoaded", function () {
     // Trouvez l'élément img
     const img = document.getElementById("pixel");
   
-    // Vérifiez si l'e-mail a déjà été ouvert en consultant le stockage local
-    const emailOpened = localStorage.getItem("emailOpened");
+    // Vérifiez si la fonction de notifications est disponible dans le navigateur
+    if ("Notification" in window) {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          // Attachez un gestionnaire d'événements load à l'image
+          img.addEventListener("load", function () {
+            // Créez une requête XMLHttpRequest lorsque l'image est chargée
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", "http://localhost:3000/pixel/open");
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(
+              JSON.stringify({
+                action: "open_email",
+                email_id: "1234567890",
+              })
+            );
   
-    // Si l'e-mail n'a pas été ouvert, effectuez la requête POST et enregistrez l'état
-    if (!emailOpened) {
-      // Créez une requête XMLHttpRequest lorsque l'image est chargée
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "http://localhost:3000/pixel/open");
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(
-        JSON.stringify({
-          action: "open_email",
-          email_id: "1234567890",
-        })
-      );
-  
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          window.alert("L'e-mail a été ouvert !");
-          // Enregistrez l'état d'ouverture de l'e-mail dans le stockage local
-          localStorage.setItem("emailOpened", "true");
-        } else {
-          window.alert("Une erreur s'est produite.");
+            xhr.onload = function () {
+              if (xhr.status === 200) {
+                // Affichez une notification lorsque le courrier électronique est ouvert
+                new Notification("L'e-mail a été ouvert !");
+              } else {
+                console.error("Une erreur s'est produite.");
+              }
+            };
+          });
         }
-      };
+      });
     }
   });
