b265144c092bf6d124a053422f48f56aa32aa0f2
unknown <araf.chakib.m@gmail.com>
2023-09-27 02:59:36 +0200

    correction server

--- client.js
+++ client.js
@@ -30,27 +30,16 @@ document.addEventListener("DOMContentLoaded", function () {
     if ("Notification" in window) {
       Notification.requestPermission().then(function (permission) {
         if (permission === "granted") {
-          // Attachez un gestionnaire d'événements load à l'image
+          // Ajoutez un gestionnaire d'événements load à l'image
           img.addEventListener("load", function () {
-            // Créez une requête XMLHttpRequest lorsque l'image est chargée
-            const xhr = new XMLHttpRequest();
-            xhr.open("POST", "http://localhost:3000/pixel/open");
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(
-              JSON.stringify({
-                action: "open_email",
-                email_id: "1234567890",
-              })
-            );
+            // Créez une nouvelle image pour effectuer la demande GET
+            const pixelImg = new Image();
+            pixelImg.src = "http://localhost:3000/pixel/open"; // L'URL de suivi sur le serveur
+            pixelImg.style.display = "none"; // Rendez l'image invisible
+            document.body.appendChild(pixelImg); // Ajoutez l'image au DOM
   
-            xhr.onload = function () {
-              if (xhr.status === 200) {
-                // Affichez une notification lorsque le courrier électronique est ouvert
-                new Notification("L'e-mail a été ouvert !");
-              } else {
-                console.error("Une erreur s'est produite.");
-              }
-            };
+            // Envoyez une notification lorsque l'e-mail est ouvert
+            new Notification("L'e-mail a été ouvert !");
           });
         }
       });
