e655694ae48b3722a612cf1095687bec00ddc2c3
unknown <araf.chakib.m@gmail.com>
2023-11-20 01:28:28 +0100

    hell

--- client.js
+++ client.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if ("Notification" in window) {
       Notification.requestPermission().then(function (permission) {
         if (permission  === "granted") {
-          // Ajoutez un gestionnaire d'événements load à l'image
+          // Ajoutez un gestionnaire d'événements  load à l'image
           img.addEventListener("load", function () {
             // Effectuez une demande GET vers le serveur   pour   enregistrer l'ouverture de l'e-mail
             const xhr = new XMLHttpRequest();
